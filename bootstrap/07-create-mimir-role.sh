ead699610dfefa77ad2afa87b04d4a061a4ba290
Noa Resare <noa@resare.com>
2026-07-01 22:43:55 +0100

    Add mimir instance to the cluster

---
+++ bootstrap/07-create-mimir-role.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+trust_policy_file="$(mktemp)"
+permission_policy_file="$(mktemp)"
+trap 'rm -f "$trust_policy_file" "$permission_policy_file"' EXIT
+
+cat >"$trust_policy_file" <<'JSON'
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "arn:aws:iam:::oidc-provider/k8s.noa.re"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "k8s.noa.re:aud": "sts.amazonaws.com",
+          "k8s.noa.re:sub": [
+            "system:serviceaccount:mimir-test:mimir",
+            "system:serviceaccount:mimir:mimir"
+          ]
+        }
+      }
+    }
+  ]
+}
+JSON
+
+cat >"$permission_policy_file" <<'JSON'
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "s3:ListBucket",
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::mimir-data"
+    },
+    {
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:DeleteObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::mimir-data/*"
+    }
+  ]
+}
+JSON
+
+trust_policy="$(jq -c . "$trust_policy_file")"
+permission_policy="$(jq -c . "$permission_policy_file")"
+
+radosgw-admin role create \
+  --rgw-realm berries \
+  --role-name mimir \
+  --assume-role-policy-doc "$trust_policy"
+
+radosgw-admin role-policy put \
+  --rgw-realm berries \
+  --role-name mimir \
+  --policy-name repo-noa-re-access \
+  --policy-doc "$permission_policy"
